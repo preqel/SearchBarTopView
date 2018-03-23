df8c1366b84df39ffe48d9e0c6143e08ee6cb191
preqel <358795715@qq.com>
2018-03-23 20:32:38 +0800

    first commit

---
+++ app/src/main/java/search/preqel/com/searchtopbarview/ClearableEditText.java
@@ -0,0 +1,106 @@
+package search.preqel.com.searchtopbarview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.gezbox.windmessage.R;
+
+/**
+ * Created by wangkang on 2018/1/4.
+ * 一个editeview自带清除功能
+ */
+
+public class ClearableEditText extends android.support.v7.widget.AppCompatEditText implements  View.OnFocusChangeListener {
+
+    /*删除按钮的引用
+    */
+    private Drawable mClearDrawable;
+
+    /* 控件是否有焦点
+    */
+    private boolean hasFoucs;
+
+    /**
+     * 是否是默认图标再左边的样式
+     */
+    private boolean isLeft = false;
+
+    /**
+     * 是否点击了enter键
+     */
+    private boolean pressSearch = false;
+
+
+    public ClearableEditText(Context context) {
+        super(context);
+    }
+
+    public ClearableEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public void init() {
+        mClearDrawable = getCompoundDrawables()[2];
+        if (mClearDrawable == null) {
+            // throw new
+            // NullPointerException("You can add drawableRight attribute in XML");
+            mClearDrawable = getResources().getDrawable(R.drawable.ic_task_create_normal);
+        }
+        setOnFocusChangeListener(this);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            if (getCompoundDrawables()[2] != null) {
+                boolean touchable = event.getX() > (getWidth() - getTotalPaddingRight()) && (event.getX() < ((getWidth() - getPaddingRight())));
+                if (touchable) {
+                    this.setText("");
+                }
+            }
+        }
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (isLeft) { // 如果是默认样式，则直接绘制
+            super.onDraw(canvas);
+        } else { // 如果不是默认样式，需要将图标绘制在中间
+            Drawable[] drawables = getCompoundDrawables();
+            if (drawables != null) {
+                Drawable drawableLeft = drawables[0];
+                if (drawableLeft != null) {
+                    float textWidth = getPaint().measureText(getHint().toString());
+                    int drawablePadding = getCompoundDrawablePadding();
+                    int drawableWidth = drawableLeft.getIntrinsicWidth();
+                    float bodyWidth = textWidth + drawableWidth + drawablePadding;
+                    canvas.translate((getWidth() - bodyWidth - getPaddingLeft() - getPaddingRight()) / 2, 0);
+                }
+            }
+            super.onDraw(canvas);
+        }
+    }
+
+
+
+    @Override
+    public void onFocusChange(View view, boolean hasFocus) {
+       if (!pressSearch && TextUtils.isEmpty(getText().toString())) {
+            isLeft = hasFocus;
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        pressSearch = (keyCode == KeyEvent.KEYCODE_ENTER);
+        return super.onKeyDown(keyCode, event);
+    }
+}
---
+++ app/src/main/java/search/preqel/com/searchtopbarview/SearchTopBarView.java
@@ -0,0 +1,139 @@
+package search.preqel.com.searchtopbarview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Created by preqel on 2018/3/23.
+ */
+
+public class SearchTopBarView extends RelativeLayout {
+
+    private Context mContext;
+    private View rl_topbar;
+    private View fl_left, fl_right;
+    protected EditText et_search;
+    private TextView tv_right;
+    private TextView tv_left;
+
+    public SearchTopBarView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mContext = context;
+        initView(context, attrs);
+    }
+
+    public SearchTopBarView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mContext = context;
+        initView(context, attrs);
+    }
+
+    private void initView(Context context, AttributeSet attrs) {
+        LayoutInflater.from(context).inflate(R.layout.search_top_bar, this);
+        rl_topbar = findViewById(R.id.rl_topbar);
+        et_search = findViewById(R.id.et_search);
+        tv_right = findViewById(R.id.tv_right);
+        tv_left = findViewById(R.id.tv_left);
+        fl_left = findViewById(R.id.fl_left);
+        fl_right = findViewById(R.id.fl_right);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.TopBarView);
+        String titleValue = typedArray.getString(R.styleable.TopBarView_titleText);
+        String rightValue = typedArray.getString(R.styleable.TopBarView_menuText);
+        Drawable rightImage = typedArray.getDrawable(R.styleable.TopBarView_menuImage);
+        int leftVisible = typedArray.getInt(R.styleable.TopBarView_leftVisible, 0);
+        int rightTextColor = typedArray.getColor(R.styleable.TopBarView_rightTextColor, Color.parseColor("#ff000000"));
+        setLeftVisible(leftVisible);
+        tv_right.setTextColor(rightTextColor);
+        if (rightValue != null && rightValue.length() > 0) {
+            tv_right.setText(rightValue);
+            tv_right.setVisibility(View.VISIBLE);
+        }
+        typedArray.recycle();
+        setNormalOnLeftClickListener();
+    }
+
+    public void addNormalTextChangeListener(TextWatcher textWatcher) {
+        et_search.addTextChangedListener(textWatcher);
+    }
+
+    public void setRrightClickListener(View.OnClickListener listener) {
+        tv_right.setOnClickListener(listener);
+    }
+
+    public String getText() {
+        if (et_search != null) {
+            return et_search.getText().toString();
+        }
+        return "";
+    }
+
+
+    private void setLeftVisible(int leftVisible) {
+        switch (leftVisible) {
+            case VISIBLE:
+                fl_left.setVisibility(VISIBLE);
+                break;
+            case INVISIBLE:
+                fl_left.setVisibility(INVISIBLE);
+                break;
+            case GONE:
+                fl_left.setVisibility(GONE);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void setTitleText(String text) {
+        setText(et_search, text);
+    }
+
+
+    private void setText(TextView view, CharSequence text) {
+        if (TextUtils.isEmpty(text)) {
+            view.setVisibility(GONE);
+        } else {
+            view.setVisibility(VISIBLE);
+            view.setText(text);
+        }
+    }
+
+    private void setNormalOnLeftClickListener() {
+        //Back
+        fl_left.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doBack(mContext);
+            }
+        });
+    }
+
+
+    private void doBack(final Context context) {
+        Class c = context.getClass();
+        try {
+            Method method = c.getMethod("onBackPressed");
+            method.invoke(context);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
